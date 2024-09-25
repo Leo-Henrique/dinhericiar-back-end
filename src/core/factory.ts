6bbd4254675916b5b625deb3f025f4f8a36bf9c2
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-09-25 18:53:23 -0300

    fix(types): change factory types

--- src/core/factory.ts
+++ src/core/factory.ts
@@ -1,13 +1,13 @@
 import { EntityInstance } from "./@types/entity";
 
 export abstract class Factory<Input> {
-  abstract make(input: Input): Promise<{
+  abstract make(input: Input): {
     input: Input;
-    entity: EntityInstance<unknown, unknown>;
-  }>;
+    entity: EntityInstance<any, any>; // eslint-disable-line @typescript-eslint/no-explicit-any
+  };
 
   abstract makeAndSave(input: Input): Promise<{
     input: Input;
-    entity: EntityInstance<unknown, unknown>;
+    entity: EntityInstance<any, any>; // eslint-disable-line @typescript-eslint/no-explicit-any
   }>;
 }
