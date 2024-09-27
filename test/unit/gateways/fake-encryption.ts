3906c4cd197bbd541022b645bff37e3641a6b7b0
leo-henrique <leonardo0507.business@gmail.com>
2024-09-27 10:42:29 -0300

    refactor(tests): remove unit tests from use cases

--- test/unit/gateways/fake-encryption.ts
+++
@@ -1,8 +0,0 @@
-import { Encryption } from "@/domain/gateways/cryptology/encryption";
-import { faker } from "@faker-js/faker";
-
-export class FakeEncryption implements Encryption {
-  async encrypt(bytes: number): Promise<string> {
-    return faker.string.hexadecimal({ length: bytes * 2, prefix: "" });
-  }
-}
--- test/unit/gateways/fake-unit-of-work.ts
+++
@@ -1,9 +0,0 @@
-import { UnitOfWork } from "@/domain/gateways/unit-of-work";
-
-export class FakeUnitOfWork implements UnitOfWork {
-  async transaction<Result>(
-    work: (session: unknown) => Promise<Result>,
-  ): Promise<Result> {
-    return work(null);
-  }
-}
