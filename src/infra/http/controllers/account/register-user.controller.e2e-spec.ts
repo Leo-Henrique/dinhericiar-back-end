632e8322fd2a1f5e56747e665d0c2d58ba6bfcb4
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-09-27 18:40:04 -0300

    test(registration): apply more assertions in the registration tests

--- src/infra/http/controllers/account/register-user.controller.e2e-spec.ts
+++ src/infra/http/controllers/account/register-user.controller.e2e-spec.ts
@@ -1,25 +1,29 @@
-import { PasswordHasher } from "@/domain/gateways/cryptology/password-hasher";
+import { UserActivationTokenEntity } from "@/domain/entities/user-activation-token.entity";
+import { EmailDispatcher } from "@/domain/gateways/email-dispatcher";
 import { AppModule } from "@/infra/app.module";
 import { DatabaseModule } from "@/infra/database/database.module";
 import { DrizzleService } from "@/infra/database/drizzle/drizzle.service";
 import { DrizzleUserActivationTokenData } from "@/infra/database/drizzle/schemas/user-activation-token.schema";
 import { DrizzleUserData } from "@/infra/database/drizzle/schemas/user.schema";
+import { faker } from "@faker-js/faker";
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 import { Test } from "@nestjs/testing";
+import { compare } from "bcryptjs";
 import { sql } from "drizzle-orm";
 import request from "supertest";
 import { UserFactory } from "test/factories/user.factory";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it, vi } from "vitest";
 import { RegisterUserControllerBody } from "./register-user.controller";
 
 describe("[Controller] POST /users", () => {
   let app: NestFastifyApplication;
   let drizzle: DrizzleService;
-  let passwordHasher: PasswordHasher;
+  let emailDispatcher: EmailDispatcher;
   let userFactory: UserFactory;
+
   let input: RegisterUserControllerBody;
   let userOnDatabase: DrizzleUserData;
 
@@ -32,7 +36,7 @@ describe("[Controller] POST /users", () => {
       new FastifyAdapter(),
     );
     drizzle = moduleRef.get(DrizzleService);
-    passwordHasher = moduleRef.get(PasswordHasher);
+    emailDispatcher = moduleRef.get(EmailDispatcher);
     userFactory = new UserFactory();
 
     const user = userFactory.make();
@@ -51,12 +55,18 @@ describe("[Controller] POST /users", () => {
     await app.close();
   });
 
-  it(`should be able to register a user`, async () => {
+  it("should be able to register a user", async () => {
+    const emailDispatcherSenderSpy = vi.spyOn(
+      emailDispatcher,
+      "sendToActivationAccount",
+    );
+
     const response = await request(app.getHttpServer())
       .post("/users")
       .send(input);
 
     expect(response.statusCode).toEqual(201);
+    expect(emailDispatcherSenderSpy).toHaveBeenCalledTimes(1);
 
     [userOnDatabase] = await drizzle.executeToGet<DrizzleUserData>(sql`
       SELECT
@@ -87,12 +97,26 @@ describe("[Controller] POST /users", () => {
     `);
 
     expect(userActivationTokenOnDatabase).toBeTruthy();
+
+    const userActivationToken = UserActivationTokenEntity.create(
+      userActivationTokenOnDatabase,
+    );
+    const activationTokenApproximateExpiration = new Date(
+      Date.now() + userActivationToken.tokenDurationInMilliseconds,
+    );
+
+    userActivationTokenOnDatabase.expiresAt.setSeconds(0, 0);
+    activationTokenApproximateExpiration.setSeconds(0, 0);
+
+    expect(userActivationTokenOnDatabase.expiresAt.getTime()).toEqual(
+      activationTokenApproximateExpiration.getTime(),
+    );
   });
 
   it("should be able to register a user with hashed password", async () => {
     expect(userOnDatabase).not.toEqual(input.password);
 
-    const isValidPassword = await passwordHasher.match(
+    const isValidPassword = await compare(
       input.password,
       userOnDatabase.password,
     );
@@ -107,5 +131,33 @@ describe("[Controller] POST /users", () => {
 
     expect(response.statusCode).toStrictEqual(409);
     expect(response.body.error).toStrictEqual("ResourceAlreadyExistsError");
+  });
+
+  describe("Input data validations", () => {
+    it("should not be able to register a user with invalid e-mail", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/users")
+        .send({
+          email: faker.lorem.sentence(),
+          password: faker.internet.password(),
+          name: faker.person.fullName(),
+        } satisfies RegisterUserControllerBody);
+
+      expect(response.statusCode).toStrictEqual(400);
+      expect(response.body.error).toStrictEqual("ValidationError");
+    });
+
+    it("should not be able to register a user with a password less than 6 characters", async () => {
+      const response = await request(app.getHttpServer())
+        .post("/users")
+        .send({
+          email: faker.internet.email(),
+          password: faker.internet.password({ length: 5 }),
+          name: faker.person.fullName(),
+        } satisfies RegisterUserControllerBody);
+
+      expect(response.statusCode).toStrictEqual(400);
+      expect(response.body.error).toStrictEqual("ValidationError");
+    });
   });
 });
