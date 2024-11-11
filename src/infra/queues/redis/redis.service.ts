83d825d538e4a1194e380541b99fc6265bad0002
Leonardo Henrique <leonardo0507.business@gmail.com>
2024-11-11 18:08:31 -0300

    fix: move random database generation function from redis

--- src/infra/queues/redis/redis.service.ts
+++ src/infra/queues/redis/redis.service.ts
@@ -1,7 +1,15 @@
 import { env } from "@/infra/env";
 import { Injectable } from "@nestjs/common";
 import { Redis } from "ioredis";
-import { generateRandomRedisDatabase } from "test/integration/generate-random-redis-database";
+
+function generateRandomRedisDatabase(min = 0, max = 15) {
+  const randomRedisDatabase = Math.floor(Math.random() * (max - min) + min);
+
+  if (randomRedisDatabase === env.REDIS_DATABASE)
+    return generateRandomRedisDatabase(min, max);
+
+  return randomRedisDatabase;
+}
 
 export const redisClient = new Redis({
   password: env.REDIS_PASSWORD,
